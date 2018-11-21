39e294c9a623d5b133654e057ab2249fa9b26925
Vinicius Sales <vinicius.2010.s@gmail.com>
2018-11-21 16:54:12 -0200

    Headers

--- src/controllers/tools.js
+++ src/controllers/tools.js
@@ -3,7 +3,8 @@ const router = express.Router();
 global.db = require('../database/db');
 
 router.get('/', (req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.append('Access-Control-Allow-Credentials', 'true');
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
