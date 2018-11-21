ca90cf47a77ca166d1ecd5eddfe5bd97f9dd08a6
Vinicius Sales <vinicius.2010.s@gmail.com>
2018-11-21 16:05:22 -0200

    Port adjustment

--- src/index.js
+++ src/index.js
@@ -18,8 +18,29 @@ app.get('/', (req, res) => {
     res.send('VUTTR API');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server running!');
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+var server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server running on ${port}!`);
 });
+
+function normalizePort(val) {
+var port = parseInt(val, 10);
+
+if (isNaN(port)) {
+    // named pipe
+    return val;
+}
+
+if (port >= 0) {
+    // port number
+    return port;
+}
+
+return false;
+}
 
 module.exports = app;
